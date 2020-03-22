8c97b8a501619dc9a4704d476b6a8a2962ebde32
albertogarciagomez <albertogarciagomez18@gmail.com>
2020-03-22 17:33:38 +0100

    Updates

---
+++ precache-manifest.13f3d5040725796b0b36244aa5c38f5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f051f151407a2652852126437ac2547f",
+    "url": "/work/index.html"
+  },
+  {
+    "revision": "9ebbbae0083b63744794",
+    "url": "/work/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a06627876983bf81e571",
+    "url": "/work/static/css/main.fe2ffb62.chunk.css"
+  },
+  {
+    "revision": "9ebbbae0083b63744794",
+    "url": "/work/static/js/2.a8ce7938.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/work/static/js/2.a8ce7938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06627876983bf81e571",
+    "url": "/work/static/js/main.2d24bbd9.chunk.js"
+  },
+  {
+    "revision": "d68b254876592963584c",
+    "url": "/work/static/js/runtime-main.63a02fd3.js"
+  }
+]);
