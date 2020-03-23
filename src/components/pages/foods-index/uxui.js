ed9e554c75c5bdc14709602ef060bf3a37548866
albertogarciagomez <albertogarciagomez18@gmail.com>
2020-03-23 16:00:43 +0100

    wwwey

--- src/components/pages/foods-index/uxui.js
+++ src/components/pages/foods-index/uxui.js
@@ -5,7 +5,24 @@ import { Container, Row } from 'react-bootstrap';
 class Content extends Component {
 
 
+
+
     render() {
+
+        $(document).ready(function() {
+            $('a[href^="#"]').click(function() {
+              var destino = $(this.hash);
+              if (destino.length == 0) {
+                destino = $('a[name="' + this.hash.substr(1) + '"]');
+              }
+              if (destino.length == 0) {
+                destino = $('html');
+              }
+              $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+              return false;
+            });
+          });
+          
         return (
             <>  
                 <Container className="jumboUxUi" id="uxuiAncla">
@@ -15,9 +32,9 @@ class Content extends Component {
                         <Row>
                             <div className="col-xs-12 col-md-6 textoCard centro">
                                 <div>
-                                <img  className="col-6" src="./img/rentoLogo.png" alt="Logo de Rento"></img>
+                                <h2>RENTO</h2>
                                 <h3 className="col-12">  Plataforma de alquiler de equipo fotográfico entre particulares   </h3>
-                                <a className="col-12" href="medium.com">See More >></a>
+                                <a className="col-12" href="">See More ></a>
                                 </div>
                             </div>
 
@@ -33,7 +50,7 @@ class Content extends Component {
                                 <div>
                                 <h2 className="col-12">WhatsApp: Avisame cuando llegues</h2>
                                 <h3 className="col-12">  Funcionalidad para WhatsApp. Envía un mensaje automático a un ser querido cuando llegas a casa  </h3>
-                                <a className="col-12" href="medium.com">See More >></a>
+                                <a className="col-12" href="https://medium.com/@albertogarciagomez18/case-study-ux-ui-week-3-funcionalidad-para-whatsapp-avisame-cuando-llegues-97dba9aa9c4f">See More ></a>
                                 </div>
                             </div>
 
--- src/components/pages/index/indexPage.js
+++ src/components/pages/index/indexPage.js
@@ -29,7 +29,7 @@ const IndexPage = () => {
                                         </div>
 
 
-                                        <a href="/contact" className="buttonHomeContact">
+                                        <a href="#contact" className="buttonHomeContact">
                                                 Contact >
                                 </a>
 
