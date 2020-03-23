b01b847b4c6542d636c5f53bc83f8e657a3511ff
albertogarciagomez <albertogarciagomez18@gmail.com>
2020-03-23 12:37:41 +0100

    eyy4

--- src/components/pages/foods-index/uxui.js
+++ src/components/pages/foods-index/uxui.js
@@ -16,7 +16,7 @@ class Content extends Component {
                             <div className="col-xs-12 col-md-6 textoCard centro">
                                 <div>
                                 <img  className="col-8 mx-auto " src="./img/rentoLogo.png" alt="Logo de Rento"></img>
-                                <h3 className="pl-3 col-8 mx-auto ">  Plataforma de alquiler de equipo fotográfico entre particulares   </h3>
+                                <h3 className="pl-1 col-8 mx-auto ">  Plataforma de alquiler de equipo fotográfico entre particulares   </h3>
                                 <a href="">See More >></a>
                                 </div>
                             </div>
@@ -27,33 +27,9 @@ class Content extends Component {
                         </Row>
                     </div>
 
-                    <div className="proyecto">
-                        <Row>
-                            <div className="col-xs-12 col-md-6">
-                                <img  className="col-8 mx-auto text-center" src="./img/rentoLogo.png" alt="Logo de Rento"></img>
-                                <h3 className="pl-3 col-8 mx-auto text-center">  Avisame cuando llegues: Funcionalidad añadida a WhatsApp   </h3>
-                                <a href="">See More >></a>
-                            </div>
-
-                            <div className="col-xs-12 col-md-6">
-                                <img  className="col-8 mx-auto text-center" src="./img/ProyectoFinal1.jpg" alt="Home de Rento" />
-                            </div>
-                        </Row>
-                    </div>
-
                 </Container>
 
-                <Container className="jumboUxUi" id="uxuiAncla">
-                    <h2>Web Developer</h2>
-
-                    <div className="proyecto">
-                        <Row>
-                            <h2></h2>
-                        </Row>
-                    </div>
-
-                    
-                </Container>
+         
             </>
         )
     }
