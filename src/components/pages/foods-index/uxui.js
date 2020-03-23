8da00385de4da8ba62f3afe1f60ea9a2ca00b4a0
albertogarciagomez <albertogarciagomez18@gmail.com>
2020-03-23 16:16:33 +0100

    123

--- src/components/pages/foods-index/uxui.js
+++ src/components/pages/foods-index/uxui.js
@@ -9,19 +9,7 @@ class Content extends Component {
 
     render() {
 
-        $(document).ready(function() {
-            $('a[href^="#"]').click(function() {
-              var destino = $(this.hash);
-              if (destino.length == 0) {
-                destino = $('a[name="' + this.hash.substr(1) + '"]');
-              }
-              if (destino.length == 0) {
-                destino = $('html');
-              }
-              $('html, body').animate({ scrollTop: destino.offset().top }, 500);
-              return false;
-            });
-          });
+        
           
         return (
             <>  
