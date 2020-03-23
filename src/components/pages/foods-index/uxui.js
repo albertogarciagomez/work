e4df26b74307973a0ea1a4d073a717e06b29fe1e
albertogarciagomez <albertogarciagomez18@gmail.com>
2020-03-23 11:26:59 +0100

    redimension

--- src/components/pages/foods-index/uxui.js
+++ src/components/pages/foods-index/uxui.js
@@ -7,37 +7,52 @@ class Content extends Component {
 
     render() {
         return (
-            <Container className="jumboUxUi" id="uxuiAncla">
-                <h2>UX/UI Designer</h2>
+            <>
+                <Container className="jumboUxUi" id="uxuiAncla">
+                    <h2>UX/UI Designer</h2>
 
-                <div className="proyecto1">
-                    <Row>
-                        <div className="col-xs-12 col-md-6">
-                            <img src="./img/rentoLogo.png" alt="Logo de Rento"></img>
-                            <h3>  Plataforma de alquiler de equipo fotográfico entre particulares   </h3>
-                            <a href="">See More >></a>
-                        </div>
+                    <div className="proyecto">
+                        <Row>
+                            <div className="col-xs-12 col-md-6">
+                                <img  className="col-8 mx-auto text-center" src="./img/rentoLogo.png" alt="Logo de Rento"></img>
+                                <h3 className="pl-3 col-8 mx-auto text-center">  Plataforma de alquiler de equipo fotográfico entre particulares   </h3>
+                                <a href="">See More >></a>
+                            </div>
 
-                        <div className="col-xs-12 col-md-6">
-                            <img src="./img/ProyectoFinal1.jpg" alt="Home de Rento"/>
-                        </div>
-                    </Row>
-                </div>
+                            <div className="col-xs-12 col-md-6">
+                                <img  className="col-8 mx-auto text-center" src="./img/ProyectoFinal1.jpg" alt="Home de Rento" />
+                            </div>
+                        </Row>
+                    </div>
 
-                <div className="proyecto1">
-                    <Row>
-                        <div className="col-xs-12 col-md-6">
-                            <img className="col-8" src="./img/rentoLogo.png"></img>
-                            <h3>  Add a feature: Avisame cuando llegues en WhatsApp   </h3>
-                            <a href="">See More >></a>
-                        </div>
+                    <div className="proyecto">
+                        <Row>
+                            <div className="col-xs-12 col-md-6">
+                                <img  className="col-8 mx-auto text-center" src="./img/rentoLogo.png" alt="Logo de Rento"></img>
+                                <h3 className="pl-3 col-8 mx-auto text-center">  Avisame cuando llegues: Funcionalidad añadida a WhatsApp   </h3>
+                                <a href="">See More >></a>
+                            </div>
 
-                        <div className="col-xs-12 col-md-6">
-                            <img className="col-8" src="./img/ProyectoFinal1.jpg" />
-                        </div>
-                    </Row>
-                </div>
-            </Container>
+                            <div className="col-xs-12 col-md-6">
+                                <img  className="col-8 mx-auto text-center" src="./img/ProyectoFinal1.jpg" alt="Home de Rento" />
+                            </div>
+                        </Row>
+                    </div>
+
+                </Container>
+
+                <Container className="jumboUxUi" id="uxuiAncla">
+                    <h2>Web Developer</h2>
+
+                    <div className="proyecto">
+                        <Row>
+                            <h2></h2>
+                        </Row>
+                    </div>
+
+                    
+                </Container>
+            </>
         )
     }
 }
