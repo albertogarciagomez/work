adb317bc58f4cdd05fef028b390a5069bdf94131
albertogarciagomez <albertogarciagomez18@gmail.com>
2020-03-25 15:58:15 +0100

    qwe

--- src/components/pages/foods-index/photovideo.js
+++ src/components/pages/foods-index/photovideo.js
@@ -7,31 +7,28 @@ import YouTube from 'react-youtube';
 
 class PhotoVideo extends Component {
 
-    constructor() {
-        super();
-        this.state = { foods: ["UX/Ui Designer", "Web Developer", "Photographer & Filmmaker"] }
-    }
     render() {
+        const opts = {
+          height: '390',
+          width: '640',
+          playerVars: { // https://developers.google.com/youtube/player_parameters
+            autoplay: 1
+          }
+        };
+    
         return (
-            <>
-               <YouTube
-                videoId={ggide-PsES4}                  // defaults -> null
-                // id={string}                       // defaults -> null
-                // className={string}                // defaults -> null
-                // containerClassName={string}       // defaults -> ''
-                // opts={obj}                        // defaults -> {}
-                // onReady={func}                    // defaults -> noop
-                // onPlay={func}                     // defaults -> noop
-                // onPause={func}                    // defaults -> noop
-                // onEnd={func}                      // defaults -> noop
-                // onError={func}                    // defaults -> noop
-                // onStateChange={func}              // defaults -> noop
-                // onPlaybackRateChange={func}       // defaults -> noop
-                // onPlaybackQualityChange={func}    // defaults -> noop
-                />
-            </>
-        )
-    }
+          <YouTube
+            videoId="ggide-PsES4"
+            opts={opts}
+            onReady={this._onReady}
+          />
+        );
+      }
+
+      _onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+      }
 }
 
 export default PhotoVideo
